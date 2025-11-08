3a8a11cf5df126e3b2ad6b909f760c2634eb5015
hydraChief <96347370+hydraChief@users.noreply.github.com>
2025-11-09 02:20:05 +0530

    blinky: using systick interrupt (#1)

--- app/src/firmware.d
+++ app/src/firmware.d
@@ -21,4 +21,9 @@ src/firmware.o: src/firmware.c \
  ../libopencm3/include/libopencm3/stm32/f4/gpio.h \
  ../libopencm3/include/libopencm3/stm32/common/gpio_common_f24.h \
  ../libopencm3/include/libopencm3/stm32/common/gpio_common_f234.h \
- ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h
+ ../libopencm3/include/libopencm3/stm32/common/gpio_common_all.h \
+ ../libopencm3/include/libopencm3/cm3/systick.h \
+ ../libopencm3/include/libopencm3/cm3/vector.h \
+ ../libopencm3/include/libopencm3/cm3/nvic.h \
+ ../libopencm3/include/libopencm3/dispatch/nvic.h \
+ ../libopencm3/include/libopencm3/stm32/f4/nvic.h
