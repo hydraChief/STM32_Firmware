3f4728a4e26d7c77114e0c100ae14b65243f6f4a
ronak <rk8875636542@gmail.com>
2025-11-12 01:21:41 +0530

    blinky using pwm and timmers

---
+++ app/src/core/timer.c
@@ -0,0 +1,30 @@
+#include "core/timer.h"
+#include <libopencm3/stm32/timer.h>
+#include <libopencm3/stm32/rcc.h>
+
+#define PRESCALER (83) // 84MHz / (83 + 1) = 1MHz timer clock : this is frequency of counter increment
+#define ARR (9999)   // 1MHz / (9999 + 1) = 100Hz PWM frequency
+
+
+void timer_setup(void){
+    rcc_periph_clock_enable(RCC_TIM2);
+
+    // High level timer config
+    timer_set_mode(TIM2, TIM_CR1_CKD_CK_INT, TIM_CR1_CMS_EDGE, TIM_CR1_DIR_UP);
+
+    //setupr PWM mode
+    timer_set_oc_mode(TIM2, TIM_OC1,TIM_OCM_PWM1);
+
+    //enable PWM output
+    timer_enable_counter(TIM2);
+    timer_enable_oc_output(TIM2, TIM_OC1);
+
+    //set prescaler and period
+    timer_set_prescaler(TIM2, PRESCALER);
+    timer_set_period(TIM2, ARR);
+}
+
+void timer_pwm_set_duty_cycle(float duty_cycle){
+    const float value= (float)ARR*(duty_cycle/100.0f);
+    timer_set_oc_value(TIM2, TIM_OC1, (uint32_t)value);
+}
--- app/src/firmware.c
+++ app/src/firmware.c
@@ -1,6 +1,8 @@
 #include <libopencm3/stm32/gpio.h>
 #include <libopencm3/stm32/rcc.h>
+
 #include "core/system.h"
+#include "core/timer.h"
 
 #define LED_PORT GPIOA
 #define LED_PIN GPIO5
@@ -8,20 +10,26 @@
 
 static void gpio_setup(void){
     rcc_periph_clock_enable(RCC_GPIOA);
-    gpio_mode_setup(LED_PORT, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, LED_PIN);
+    gpio_mode_setup(LED_PORT, GPIO_MODE_AF, GPIO_PUPD_NONE, LED_PIN);
+    gpio_set_af(LED_PORT, GPIO_AF1, LED_PIN);
 }
 
 
 int main(void){
     system_setup();
     gpio_setup();
-
+    timer_setup();
     uint64_t start_time=system_get_ticks();
-
+    float duty_cycle=0.0f;
+    timer_pwm_set_duty_cycle(duty_cycle);
     while (1){
-        if(system_get_ticks()-start_time>=5000){
+        if(system_get_ticks()-start_time>=10){
             start_time=system_get_ticks();
-            gpio_toggle(LED_PORT, LED_PIN);
+            duty_cycle+=1.0f;
+            if(duty_cycle>100.0f){
+                duty_cycle=0.0f;
+            }
+            timer_pwm_set_duty_cycle(duty_cycle);
         }
     }
     return 0;
